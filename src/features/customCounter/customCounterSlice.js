214058504669e8c24bf89f1b2a7fa5986ed79f8e
yuktnk <yuki0wl@gmail.com>
2022-08-11 16:52:34 +0900

    RTKにテスト用のSliceを新規作成

---
+++ src/features/customCounter/customCounterSlice.js
@@ -0,0 +1,84 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    value: 0,
+    mode: 0,
+    username: "",
+};
+
+const sleep = (msec) => {
+    const start = new Date();
+    while (new Date() - start < msec) {}
+};
+
+export const fetchDummy = createAsyncThunk("fetch/dummy", async (num) => {
+    await sleep(2000); // 2秒間待つ
+    return num;
+});
+
+export const fetchJSON = createAsyncThunk("fetch/api", async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users/1");
+    const { username } = res.data;
+    return username;
+});
+
+export const customCounterSlice = createSlice({
+    name: "customCounter",
+    initialState,
+
+    reducers: {
+        increment: (state) => {
+            switch (state.mode) {
+                case 0:
+                    state.value += 1;
+                    break;
+                case 1:
+                    state.value += 100;
+                    break;
+                case 2:
+                    state.value += 10000;
+                    break;
+                default:
+                    break;
+            }
+        },
+        decrement: (state) => {
+            state.value -= 1;
+        },
+
+        incrementByAmount: (state, action) => {
+            switch (state.mode) {
+                case 0:
+                    state.value += action.payload;
+                    break;
+                case 1:
+                    state.value += action.payload * 100;
+                    break;
+                case 2:
+                    state.value += action.payload * 10000;
+                    break;
+                default:
+                    break;
+            }
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchDummy.fulfilled, (state, action) => {
+            state.value = 100 + action.payload;
+        });
+        builder.addCase(fetchDummy.rejected, (state, action) => {
+            state.value = 100 - action.payload;
+        });
+        builder.addCase(fetchJSON.fulfilled, (state, action) => {
+            state.username = action.payload;
+        });
+    },
+});
+
+export const { increment, decrement, incrementByAmount } = customCounterSlice.actions;
+
+export const selectCount = (state) => state.counter.value;
+export const selectUsername = (state) => state.counter.username;
+
+export default customCounterSlice.reducer;
